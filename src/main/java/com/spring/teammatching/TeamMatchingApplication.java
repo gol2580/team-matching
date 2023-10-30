bc2946e61c30255bedc9fb409dd6abbffcc84410
gol2580 <gol11971197@gmail.com>
2023-10-30 18:12:07 +0900

    Initial commit

---
+++ src/main/java/com/spring/teammatching/TeamMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.teammatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMatchingApplication.class, args);
+	}
+
+}
